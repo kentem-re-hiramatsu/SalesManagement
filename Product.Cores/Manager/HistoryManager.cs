f8f1adc7cb7145e1abbbeca0eb44f8ee18a838a5
re-hiramatsu <re-hiramatsu>
2024-06-20 13:47:15 +0900

    HistoryManager.cs実装

---
+++ Product.Cores/Manager/HistoryManager.cs
@@ -0,0 +1,16 @@
+﻿using Product.Cores.Model;
+using System.Collections.Generic;
+
+namespace Product.Cores.Manager
+{
+    public class HistoryManager
+    {
+        private List<Sale> _historyList = new List<Sale>();
+
+        public IReadOnlyCollection<Sale> HistoryList { get { return _historyList; } }
+
+        public void Add(Sale sale) => _historyList.Add(sale);
+
+        public Sale GetSale(int index) => _historyList[index];
+    }
+}
