ffb47338334d18e8aa73df5971d01af458f54846
re-hiramatsu <re-hiramatsu>
2024-06-20 13:46:56 +0900

    HistoryManagerTest.cs実装

---
+++ SalesManagementTest/ManagerTest/HistoryManagerTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Product.Cores.Manager;
+using Product.Cores.Model;
+
+namespace SalesManagementTest.ManagerTest
+{
+    [TestClass]
+    public class HistoryManagerTest
+    {
+        [TestMethod]
+        public void HistoryTest()
+        {
+            var _historyMana = new HistoryManager();
+            var mikanPurchase = new Purchase("みかん", "6/24", 5, 100);
+            var bananaPurchase = new Purchase("ばなな", "6/24", 5, 100);
+            var mikanSale = new Sale(200, mikanPurchase);
+            var bananaSale = new Sale(200, bananaPurchase);
+
+            Assert.AreEqual(0, _historyMana.HistoryList.Count);
+
+            _historyMana.Add(mikanSale);
+            _historyMana.Add(bananaSale);
+
+            Assert.AreEqual(2, _historyMana.HistoryList.Count);
+            Assert.AreEqual("みかん", _historyMana.GetSale(0).Purchase.ProductName);
+            Assert.AreEqual("ばなな", _historyMana.GetSale(1).Purchase.ProductName);
+        }
+    }
+}
