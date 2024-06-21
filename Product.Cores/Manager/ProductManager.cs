82b22e29dad19bbc7a809d3e959d99192d336a81
re-hiramatsu <re-hiramatsu>
2024-06-21 14:42:06 +0900

    購入履歴マネージャーと商品マネージャーに修正

---
+++ Product.Cores/Manager/ProductManager.cs
@@ -0,0 +1,16 @@
+﻿using Product.Cores.Model;
+using System.Collections.Generic;
+
+namespace Product.Cores.Manager
+{
+    public class ProductManager
+    {
+        private List<Purchase> _productList = new List<Purchase>();
+
+        public IReadOnlyCollection<Purchase> ProductList { get { return _productList; } }
+
+        public void Add(Purchase purchase) => _productList.Add(purchase);
+
+        public Purchase GetPurchase(int index) => _productList[index];
+    }
+}
---
+++ Product.Cores/Manager/SalesHistoryManager.cs
@@ -0,0 +1,27 @@
+﻿using Product.Cores.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Product.Cores.Manager
+{
+    public class SalesHistoryManager
+    {
+        private List<Sale> _historyList = new List<Sale>();
+
+        public IReadOnlyCollection<Sale> HistoryList { get { return _historyList; } }
+
+        public void Add(Sale sale) => _historyList.Add(sale);
+
+        public Sale GetSale(int index) => _historyList[index];
+
+        /// <summary>
+        /// 売上合計金額
+        /// </summary>
+        public int GetTotalSalesAmount() => _historyList.Sum(sale => sale.GetSalesAmount());
+
+        /// <summary>
+        /// 利益合計金額
+        /// </summary>
+        public int GetTotalIncomeAmount() => _historyList.Sum(sale => sale.GetIncomeAmount());
+    }
+}
