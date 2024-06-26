50caa3e4317391d67e948fea2284fe5b92ea8c48
re-hiramatsu <re-hiramatsu>
2024-06-26 16:45:21 +0900

    同じ商品と販売日の場合、カートに追加するときにまとめる機能のテスト実装

--- SalesManagementTest/ManagerTest/SalesCartManagerTest.cs
+++ SalesManagementTest/ManagerTest/SalesCartManagerTest.cs
@@ -25,6 +25,20 @@ namespace SalesManagementTest.ManagerTest
         }
 
         [TestMethod]
+        public void SameAddTest()
+        {
+            Assert.AreEqual(0, salesmana.CartList.Count);
+
+            var banana = new Purchase("ばなな", 100, 200, 10, new DateTime(2024, 06, 21));
+            salesmana.Add(new Sale(2, new DateTime(2024, 06, 22), banana));
+            salesmana.Add(new Sale(3, new DateTime(2024, 06, 21), banana));
+            salesmana.Add(new Sale(3, new DateTime(2024, 06, 22), banana));
+
+            Assert.AreEqual(2, salesmana.CartList.Count);
+            Assert.AreEqual(5, salesmana.GetSale(0).SaleQuantity);
+        }
+
+        [TestMethod]
         public void Remove()
         {
             var banana = new Purchase("ばなな", 100, 200, 10, new DateTime(2024, 06, 21));
