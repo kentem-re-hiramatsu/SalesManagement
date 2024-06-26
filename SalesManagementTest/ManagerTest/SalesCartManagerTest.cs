498ea610f3cc9b92e5d3056684bc850f04ad0b90
re-hiramatsu <re-hiramatsu>
2024-06-26 14:22:09 +0900

    カート機能テスト実装

---
+++ SalesManagementTest/ManagerTest/SalesCartManagerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Product.Cores.Managers;
+using Product.Cores.Models;
+using System;
+
+namespace SalesManagementTest.ManagerTest
+{
+    [TestClass]
+    public class SalesCartManagerTest
+    {
+        SalesCartManager salesmana = new SalesCartManager();
+
+        [TestMethod]
+        public void AddTest()
+        {
+            Assert.AreEqual(0, salesmana.CartList.Count);
+
+            var banana = new Purchase("ばなな", 100, 200, 10, new DateTime(2024, 06, 21));
+            salesmana.Add(new Sale(2, new DateTime(2024, 06, 22), banana));
+            salesmana.Add(new Sale(3, new DateTime(2024, 06, 21), banana));
+
+            Assert.AreEqual(2, salesmana.CartList.Count);
+            Assert.AreEqual(new DateTime(2024, 06, 22), salesmana.GetSale(0).SaleDateTime);
+            Assert.AreEqual(new DateTime(2024, 06, 21), salesmana.GetSale(1).SaleDateTime);
+        }
+    }
+}
