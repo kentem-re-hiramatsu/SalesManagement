f1f6708236e0261ca1a167381b4386054e4318d3
re-hiramatsu <re-hiramatsu>
2024-06-27 11:48:12 +0900

    Productモデルテストケース作成

---
+++ SalesManagementTest/ModelTest/ProductTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Products.Cores.Models;
+
+namespace SalesManagementTest.ModelTest
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void ProductGetTest()
+        {
+            var product = new Product("りんご");
+            Assert.AreEqual("りんご", product.Name);
+        }
+    }
+}
