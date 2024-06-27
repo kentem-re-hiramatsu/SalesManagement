b63d25b2d582e763bfaf9e25d6536f33463c5a29
re-hiramatsu <re-hiramatsu>
2024-06-27 11:52:16 +0900

    同じ商品名のエラーメッセージ追加

--- Product.Cores/Consts.cs
+++ Product.Cores/Consts.cs
@@ -1,4 +1,4 @@
-﻿namespace Product.Cores
+﻿namespace Products.Cores
 {
     public class Consts
     {
@@ -6,6 +6,7 @@
         public static readonly string PURCHASE_OVER_PRICE_ERROR_MESSAGE = "仕入価格が販売価格と同じか上回っています。もう一度入力してください。";
         public static readonly string INPUT_ERROR_MESSAGE = "不正な値が入力されました。";
         public static readonly string LESS_THAN_SALEDAY_ERROR_MESSAGE = "販売日が仕入日を下回っています。もう一度入力してください";
+        public static readonly string SAME_PRODUCT_NAME_ERROR_MESSAGE = "同じ商品名が登録されています。もう一度入力してください";
 
         public const int CHECK_BOX_COLUMN = 0;
         public const int PRODUCT_NAME_COLUMN = 1;
