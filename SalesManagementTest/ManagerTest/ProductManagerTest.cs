0ec8209066cdcbb1099cc47407e310e88e21b2f4
re-hiramatsu <re-hiramatsu>
2024-06-27 11:49:46 +0900

    ProductManaのテスト実装

---
+++ SalesManagementTest/ManagerTest/ProductManagerTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Products.Cores.Managers;
+using Products.Cores.Models;
+using System;
+
+namespace SalesManagementTest.ManagerTest
+{
+    [TestClass]
+    public class ProductManagerTest
+    {
+        [TestMethod]
+        public void ProductManaTest()
+        {
+            var productMana = new ProductManager();
+            Assert.AreEqual(0, productMana.ProductList.Count);
+
+            productMana.Add(new Product("バナナ"));
+            productMana.Add(new Product("みかん"));
+
+            Assert.AreEqual(2, productMana.ProductList.Count);
+            Assert.AreEqual("バナナ", productMana.GetProduct(0).Name);
+            Assert.AreEqual("みかん", productMana.GetProduct(1).Name);
+        }
+
+        [TestMethod]
+        public void SameProductErrorTest()
+        {
+            var productMana = new ProductManager();
+            productMana.Add(new Product("バナナ"));
+
+            Assert.ThrowsException<Exception>(() => productMana.Add(new Product("バナナ")));
+        }
+    }
+}
