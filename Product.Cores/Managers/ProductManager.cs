66572a1cc7466ee8d2940e93aeb198f511ba5d6b
re-hiramatsu <re-hiramatsu>
2024-06-27 11:50:44 +0900

    Productモデルとマネージャー実装

---
+++ Product.Cores/Managers/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Products.Cores.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Products.Cores.Managers
+{
+    public class ProductManager
+    {
+        private List<Product> _productList = new List<Product>();
+
+        public IReadOnlyCollection<Product> ProductList { get { return _productList; } }
+
+        public void Add(Product product)
+        {
+            foreach (var productItem in _productList)
+            {
+                if (product.Name == productItem.Name)
+                    throw new Exception(Consts.SAME_PRODUCT_NAME_ERROR_MESSAGE);
+            }
+            _productList.Add(product);
+        }
+
+        public Product GetProduct(int index)
+        {
+            return _productList[index];
+        }
+    }
+}
---
+++ Product.Cores/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Products.Cores.Models
+{
+    public class Product
+    {
+        public string Name { get; }
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+    }
+}
