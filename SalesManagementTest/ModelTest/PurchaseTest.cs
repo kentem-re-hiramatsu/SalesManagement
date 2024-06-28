caf95d39c4886c1d277d51be415595536cfba349
re-hiramatsu <re-hiramatsu>
2024-06-28 14:35:27 +0900

    境界値に変更

--- SalesManagementTest/ModelTest/PurchaseTest.cs
+++ SalesManagementTest/ModelTest/PurchaseTest.cs
@@ -11,12 +11,12 @@ namespace SalesManagementTest.ModelTest
         public void PurchaseConstructorTest()
         {
             var ringo = new Product("りんご");
-            var purchase = new Purchase(ringo, 100, 200, 20, new DateTime(2024, 06, 21));
+            var purchase = new Purchase(ringo, 100, 101, 20, new DateTime(2024, 06, 21));
             var purchaseDateTime = (new DateTime(2024, 06, 21));
 
             Assert.AreEqual("りんご", purchase.Product.Name);
             Assert.AreEqual(100, purchase.PurchasePrice);
-            Assert.AreEqual(200, purchase.SalePrice);
+            Assert.AreEqual(101, purchase.SalePrice);
             Assert.AreEqual(20, purchase.StockQuantity);
             Assert.AreEqual(purchaseDateTime, purchase.PurchaseDateTime);
 
@@ -26,7 +26,7 @@ namespace SalesManagementTest.ModelTest
             Assert.ThrowsException<Exception>(() => new Purchase(ringo, 100, 0, 20, new DateTime(2024, 06, 21)));
             Assert.ThrowsException<Exception>(() => new Purchase(ringo, 100, -1, 20, new DateTime(2024, 06, 21)));
 
-            Assert.ThrowsException<Exception>(() => new Purchase(ringo, 200, 200, 20, new DateTime(2024, 06, 21)));
+            Assert.ThrowsException<Exception>(() => new Purchase(ringo, 100, 100, 20, new DateTime(2024, 06, 21)));
         }
     }
 }
