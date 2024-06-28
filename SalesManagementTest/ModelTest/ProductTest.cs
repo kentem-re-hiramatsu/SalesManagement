f5c5b1d1666e36920fe38e2216f29cddf33f8df7
re-hiramatsu <re-hiramatsu>
2024-06-28 09:19:06 +0900

    Idのテストケース作成

--- SalesManagementTest/ModelTest/ProductTest.cs
+++ SalesManagementTest/ModelTest/ProductTest.cs
@@ -10,7 +10,10 @@ namespace SalesManagementTest.ModelTest
         public void ProductGetTest()
         {
             var product = new Product("りんご");
+            product.Id = 0;
+
             Assert.AreEqual("りんご", product.Name);
+            Assert.AreEqual(0, product.Id);
         }
     }
 }
